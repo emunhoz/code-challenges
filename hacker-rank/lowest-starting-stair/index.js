7cf9e7125d1a143778b26c8886be2646b9fd0868
emunhoz <edermunhozsantos@gmail.com>
2022-06-12 16:09:55 -0300

    chore: use for in arr lowest stating stair challenge

--- hacker-rank/lowest-starting-stair/index.js
+++ hacker-rank/lowest-starting-stair/index.js
@@ -2,7 +2,7 @@ export function findLowestStartingStair(arr) {
   let min = Number.MAX_VALUE
   let sum = 0
 
-  for (let i = 0; i < arr.length; i++) {
+  for (const i in arr) {
     sum = sum + arr[i]
     min = Math.min(sum, min)
   }
